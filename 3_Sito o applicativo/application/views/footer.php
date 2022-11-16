4a27f4ea75b3a09369c49aabba40fc0a9c8a8db0
Andrea <andrea.curti@samtrevano.ch>
2022-11-16 16:22:29 +0100

    16.11.2022

---
+++ 3_Sito o applicativo/application/views/footer.php
@@ -0,0 +1,15 @@
+<!--        <footer class="bg-light text-light text-center text-lg-start fixed-bottom">-->
+<!--        <div class="text-center p-2" style="background-color: rgba(0, 0, 0, 0.5);">-->
+<!--            © 2022 Copyright:-->
+<!--            <a class="text-light">Andrea Curti</a>-->
+<!--        </div>-->
+<!--        </footer>-->
+        <script type="text/javascript" src="<?php echo URL; ?>application/public/mdb/js/mdb.min.js"></script>
+        <script type="text/javascript" src="<?php echo URL; ?>application/public/js/boostrap.min.js"></script>
+        <script type="text/javascript" src="<?php echo URL; ?>application/public/js/boostrap.js"></script>
+        <script type="text/javascript" src="<?php echo URL; ?>application/public/js/boostrap.esm.min.js"></script>
+        <script type="text/javascript" src="<?php echo URL; ?>application/public/js/boostrap.esm.js"></script>
+        <script type="text/javascript" src="<?php echo URL; ?>application/public/js/boostrap.bundle.min.js"></script>
+        <script type="text/javascript" src="<?php echo URL; ?>application/public/js/boostrap.bundle.js"></script>
+    </body>
+</html>
---
+++ 3_Sito o applicativo/application/views/users/deleteUser.php
@@ -0,0 +1,38 @@
+<div class="container d-flex justify-content-center py-5 h-100 text-center">
+    <div class="row align-items-center mb-5">
+        <div class="col-lg-12" style="z-index: 10">
+            <h1 class="display-5 fw-bold ls-tight" style="color: hsl(218, 81%, 95%)">
+                Elimina utente
+            </h1>
+        </div>
+
+        <div class="card bg-glass mb-5">
+            <div class="card-body px-4 pt-5 pb-4 px-md-5 ">
+                <form action="<?php echo URL ?>users/deleteUser" method="POST"
+                      onsubmit="return confirm('Confermo di voler eliminare questo utente?');">
+
+                    <!-- Select user input -->
+                    <div class="form-group mb-4">
+                        <label for="selectUser" class="control-label pull-right h4">Scegli un utente</label><br><br>
+                        <select name="idUser" id="selectUser" class="form-select" style="background: transparent; color: #4f4f4f">
+                            <?php if(isset($data['users'])){ foreach($data['users'] as $user): ?>
+                                <option value="<?php echo $user["id"]; ?>" <?php echo (isset($data['lastId']) && $data['lastId'] == $user["id"] ? "SELECTED" : ""); ?>><?php echo $user["email"]; ?></option>
+                            <?php endforeach; }?>
+                        </select>
+                    </div><br>
+
+                    <!-- Submit button -->
+                    <div class="form-outline w-50 mx-auto">
+                        <button type="submit" class="btn btn-primary btn-block mb-4">Elimina</button>
+                    </div>
+                    <div class="form-outline d-flex justify-content-center">
+                        <?php if(isset($data['error'])){ ?>
+                            <label id="errorLogin" class="form-label alert alert-danger"> <?php echo $data['error'] ?></label>
+                        <?php } ?>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
