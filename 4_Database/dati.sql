e991244272b2de9e939ab38e13752fd7a5f66ed5
Andrea <andrea.curti@samtrevano.ch>
2022-11-30 16:12:45 +0100

    30.11.2022 V1

--- 4_Database/dati.sql
+++ 4_Database/dati.sql
@@ -4,19 +4,19 @@ insert into user values ('admin@gmail.com','b85168fa6bfe3208355d0c74f0c8dd6379f2
 insert into user values ('andrea.curti@gmail.com','7aa53a4d20a1882d92fab30f2b21744c87e7a14ff6d48e4d2a24b282adafc384');
 
 # Client
-insert into client(name, surname, address, phone) values ("Mattia", "Pasquini", "mattia.pasquini@gmail.com", 0769999999);
-insert into client(name, surname, address, phone) values ("Andrea", "Masciocchi", "andrea.masciocchi@gmail.com", 0769999998);
-insert into client(name, surname, address, phone) values ("Manuel", "Grosso", "manuel.grosso@gmail.com", 0769999997);
+insert into client(name, surname, email, phone) values ("Mattia", "Pasquini", "mattia.pasquini@gmail.com", 0769999999);
+insert into client(name, surname, email, phone) values ("Andrea", "Masciocchi", "andrea.masciocchi@gmail.com", 0769999998);
+insert into client(name, surname, email, phone) values ("Manuel", "Grosso", "manuel.grosso@gmail.com", 0769999997);
 
 # product
-insert into product(name, cost) values ("shampoo1", 8.50);
-insert into product(name, cost) values ("shampoo2", 7.00);
-insert into product(name, cost) values ("shampoo3", 9.50);
+insert into product(name, cost, active) values ("shampoo1", 8.50, 1);
+insert into product(name, cost, active) values ("shampoo2", 7.00, 1);
+insert into product(name, cost, active) values ("shampoo3", 9.50, 1);
 
 # service
-insert into service(name, cost) values ("taglio1", 8.50);
-insert into service(name, cost) values ("taglio2", 7.00);
-insert into service(name, cost) values ("taglio3", 9.50);
+insert into service(name, cost, active) values ("taglio1", 8.50, 1);
+insert into service(name, cost, active) values ("taglio2", 7.00, 1);
+insert into service(name, cost, active) values ("taglio3", 9.50, 1);
 
 # method
 insert into method(name) values ("cash");
--- 4_Database/script.sql
+++ 4_Database/script.sql
@@ -3,52 +3,54 @@ CREATE DATABASE gestione_salone;
 USE gestione_salone;
 
 CREATE TABLE user(
-	email VARCHAR(50) PRIMARY KEY,
-    password VARCHAR(64)
+	email VARCHAR(50) PRIMARY KEY NOT NULL,
+    password VARCHAR(64) NOT NULL
 );
 
 CREATE TABLE client(
 	id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(20),
-    surname VARCHAR(20),
-    address VARCHAR(30),
-    phone INT(10)
+    name VARCHAR(20) NOT NULL,
+    surname VARCHAR(20) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    phone INT(10) NOT NULL
 );
 
 CREATE TABLE product(
 	id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(20),
-    cost DECIMAL
+    name VARCHAR(20) NOT NULL,
+    cost DECIMAL NOT NULL,
+    active INT NOT NULL
 );
 
 CREATE TABLE service(
 	id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(20),
-    cost DECIMAL
+    name VARCHAR(20) NOT NULL,
+    cost DECIMAL NOT NULL,
+    active INT NOT NULL
 );
 
 CREATE TABLE method(
-	id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(20)
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(20) NOT NULL
 );
 
 
 CREATE TABLE client_buys_service(
-	client_id int,
-    service_id int,
-    method_id int,
-    date date,
-    FOREIGN KEY (client_id) REFERENCES client(id),
+	client_id int NOT NULL,
+    service_id int NOT NULL,
+    method_id int NOT NULL,
+    date date NOT NULL,
+    FOREIGN KEY (client_id) REFERENCES client(id) ON DELETE CASCADE,
     FOREIGN KEY (service_id) REFERENCES service(id),
     FOREIGN KEY (method_id) REFERENCES method(id)
 );
 
 CREATE TABLE client_buys_product(
-	client_id int,
-    product_id int,
-    method_id int,
-    date date,
-	FOREIGN KEY (client_id) REFERENCES client(id),
+	client_id int NOT NULL,
+    product_id int NOT NULL,
+    method_id int NOT NULL,
+    date date NOT NULL,
+	FOREIGN KEY (client_id) REFERENCES client(id) ON DELETE CASCADE,
     FOREIGN KEY (product_id) REFERENCES product(id),
     FOREIGN KEY (method_id) REFERENCES method(id)
 );
