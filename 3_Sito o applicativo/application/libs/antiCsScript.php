77693e7cd60c6a14bcd3a613a7e62cab1131dba4
MattiaPasquini <mattia.pasquini@samtrevano.ch>
2023-01-02 23:13:03 +0100

    fix antiscript

--- 3_Sito o applicativo/application/libs/antiCsScript.php
+++ 3_Sito o applicativo/application/libs/antiCsScript.php
@@ -4,31 +4,17 @@
 	 */
 	abstract class AntiCsScript
 	{
-         /**
-         * Questo metodo permette di controllare se una stringa contenga 
-         * parti di script che potrebbero essere eseguite involontariamente
-         * 
-         * @param String $text -> la stringa da controllare
-         */
-		public static function checkAntiScript($text){
-			if(strpos($text, '<script')){
-                $text = str_replace("<script", "", $text);
+            /**
+             * Questo metodo permette di rimuovere spazi vuoti, gli slash, 
+             * e caratteri speciali da una stringa
+             * 
+             * @param String $data -> la stringa da controllare
+             */
+            public static function check($data) {
+                $data = trim($data);
+                $data = stripslashes($data);
+                $data = htmlspecialchars($data);
+                return $data;
             }
-            if($text == ""){ throw new Exception("Completare tutti i campi"); }
-            return $text;
-		}
-
-        /**
-         * Questo metodo permette di rimuovere spazi vuoti, gli slash, 
-         * e caratteri speciali da una stringa
-         * 
-         * @param String $data -> la stringa da controllare
-         */
-        public static function check($data) {
-            $data = trim($data);
-            $data = stripslashes($data);
-            $data = htmlspecialchars($data);
-            return AntiCsScript::checkAntiScript($data);
-        }
 	}
 ?>
